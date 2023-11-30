82520a559ec4a07246b09ef666e4a5c7417c456e
dimitril <dimitrilopez01@gmail.com>
2023-11-30 12:50:45 -0500

    Add house_request NLP

--- part2.pl
+++ part2.pl
@@ -1,6 +1,11 @@
 :- [hogwarts].
 :- [part2Helper].
 
+% NOTE: This matches on a wild card. For the life of me I wasnt able to figure
+% out how to match on the "blank" character.
+period --> [_].
+comma --> [_].
+
 person(Person) --> [Person], {houseOf(_, Person)}.
 item(Item) --> [Item], {catch(char_type(Item, lower), _, fail)}.
 value(Value) --> [Value], {
@@ -18,6 +23,18 @@ value(Value) --> [Value], {
                  }.
 has_item(Person, Item, Cost, Volume) --> person(Person), [has, item], item(Item), [that, costs], value(Cost), [dollars, _, and, occupies], value(Volume), [cubic, feet, _].
 
+house(House) --> [House], {
+                     houseOf(House, _),
+                     write("Parsed House: "), write(House), nl
+                 }.
+comparison --> [less, than] | [greater, than].
+attribute(money) --> [total, price].
+attribute(volume) --> [cubic, feet].
+unit(money) --> [dollars].
+unit(volume) --> [cubic, feet].
+attribute_value --> attribute(UnitType), comparison, value(_), unit(UnitType).
+house_request --> house(House), [house, wants], attribute_value, [and], attribute_value, period.
+
 parse_lines([]) :-
     write("Finished parsing all the lines."),
     write("\n").
@@ -25,11 +42,14 @@ parse_lines([Sentence | _]) :-
     write("Original Sentence: "),
     write(Sentence),
     write("\n"),
-    phrase(has_item(Person, Item, Cost, Volume), Sentence),
-    write("Person: "), write(Person), nl,
-    write("Item: "), write(Item), nl,
-    write("Cost: "), write(Cost), nl,
-    write("Volume: "), write(Volume), nl,
+    %% phrase(has_item(Person, Item, Cost, Volume), Sentence),
+    %% write("Person: "), write(Person), nl,
+    %% write("Item: "), write(Item), nl,
+    %% write("Cost: "), write(Cost), nl,
+    %% write("Volume: "), write(Volume), nl,
+
+    phrase(house_request, Sentence),
+    write("Was able to parse!"), nl,
     write("\n").
 
 % Hints:
