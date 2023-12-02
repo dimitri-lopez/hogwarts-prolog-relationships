4dd7ebdee539000712b96b10588fbb70164f8017
Dylan Le <self@dylanle.dev>
2023-12-02 02:49:58 -0500

    fix: convert atoms to ints, and do good constraint

--- part2.pl
+++ part2.pl
@@ -22,13 +22,13 @@ value(Value) --> [Value], {
                  }.
 has_item(Person, Item, Cost, Volume) -->
     person(Person), [has, item], item(Item), [that, costs], value(Cost), [dollars], comma,
-    [and, occupies], value(Volume), [cubic, feet], period,
-    {
-        write("Person: "), write(Person), nl,
-        write("Item: "), write(Item), nl,
-        write("Cost: "), write(Cost), nl,
-        write("Volume: "), write(Volume), nl
-    }.
+    [and, occupies], value(Volume), [cubic, feet], period.
+%{
+%        write("Person: "), write(Person), nl,
+%        write("Item: "), write(Item), nl,
+%        write("Cost: "), write(Cost), nl,
+%        write("Volume: "), write(Volume), nl
+%    }.
 
 house(House) --> [House], {houseOf(House, _)}.
 comparison(less_than) --> [less, than].
@@ -41,23 +41,25 @@ attribute_value(UnitType, Comparison, Value) --> attribute(UnitType), comparison
 house_request(House, UnitType1, Comparison1, Value1, UnitType2, Comparison2, Value2) -->
     house(House), [house, wants], attribute_value(UnitType1, Comparison1, Value1), [and], attribute_value(UnitType2, Comparison2, Value2), period.
 
-parse_lines([]) :-
-    write("Finished parsing all the lines."), nl.
+parse_lines([]).
+    %write("Finished parsing all the lines."), nl.
 parse_lines([Sentence | LinesToParse]) :-
-    write("Original Sentence: "), write(Sentence), nl,
+    %write("Original Sentence: "), write(Sentence), nl,
     (
         parse_item(Sentence) ->
-        true ;
+        true;
         parse_house_request(Sentence)
     ),
     parse_lines(LinesToParse).
 
 parse_item(Sentence) :-
-    phrase(has_item(Person, Item, Cost, Volume), Sentence),
+    phrase(has_item(Person, Item, Cost_Atom, Volume_Atom), Sentence),
+    atom_number(Cost_Atom, Cost),
+    atom_number(Volume_Atom, Volume),
     assert(
         item_info(Item, Person, Cost, Volume)
-    ),
-    write("Was able to parse item!"), nl.
+    ).
+    %write("Was able to parse item!"), nl.
 
 % Generate all subsets of a list
 subset([], []).
@@ -67,29 +69,43 @@ subset([_|T],T1):-
   subset(T, T1).
 
 parse_house_request(Sentence) :-
-    phrase(house_request(House, UnitType1, Comparison1, Value1,
-                         UnitType2, Comparison2, Value2),
+    phrase(house_request(House, UnitType1, Comparison1, Value1_Atom,
+                         UnitType2, Comparison2, Value2_Atom),
            Sentence),
-    write("House: "), write(House), nl,
-    write("UnitType1: "), write(UnitType1), nl,
-    write("Comparison1: "), write(Comparison1), nl,
-    write("Value1: "), write(Value1), nl,
-    write("UnitType2: "), write(UnitType2), nl,
-    write("Comparison2: "), write(Comparison2), nl,
-    write("Value2: "), write(Value2), nl,
-    write("Was able to parse house request!"), nl,
-
-    findall([Item, Person, Cost, Volume], item_info(Item, Person, Cost, Volume), AllItems),
-    write("AllItems: "), write(AllItems), nl,
-    subset(AllItems, SelectedItems),
-    findall(Cost, (member(Item, SelectedItems), nth0(2, Item, Cost)), Costs),
-    write("Costs: "), write(Costs), nl,
+    atom_number(Value1_Atom, Value1),
+    atom_number(Value2_Atom, Value2),
+    %write("House: "), write(House), nl,
+    %write("UnitType1: "), write(UnitType1), nl,
+    %write("Comparison1: "), write(Comparison1), nl,
+    %write("Value1: "), write(Value1), nl,
+    %write("UnitType2: "), write(UnitType2), nl,
+    %write("Comparison2: "), write(Comparison2), nl,
+    %write("Value2: "), write(Value2), nl,
+    %write("Was able to parse house request!"), nl,
+    write(House), nl,
+    findall([Item, Cost, Volume], (item_info(Item, Person, Cost, Volume), houseOf(House, Person)), AllItems),
+    %write("AllItems: "), write(AllItems), nl,
+    findall(_,(
+      subset(AllItems, SelectedItems),
+      fulfills_constraint(UnitType1, Comparison1, Value1, SelectedItems),
+      fulfills_constraint(UnitType2, Comparison2, Value2, SelectedItems),
+      findall(ItemCost, (member(Selected, SelectedItems), nth0(1, Selected, ItemCost) ), ListCosts),
+      findall(ItemVolume, (member(Selected, SelectedItems), nth0(2, Selected, ItemVolume) ), ListVolumes),
+      findall(ItemName, (member(Selected, SelectedItems), nth0(0, Selected, ItemName) ), ListNames),
+      sum_list(ListCosts, TotalCost), % LINE IN QUESTION
+      sum_list(ListVolumes, TotalVolume), % LINE IN QUESTION
+      write('\t['),write(TotalCost),write(","),write(TotalVolume),write("]:"),write(ListNames),nl
+      %write("Was able to find something that meets all the constraints"), nl,
+      %write("Items: "), write(SelectedItems), nl),
+      ),
+    _
+    ).
+    %write("Costs: "), write(Costs), nl,
     % NOTE: All I want to do on this line is sum up the costs in a list. Not
     % sure why uncommenting this line causes AllItems to infinitely grow in
     % size. Figure out this bug and you are basically done. See constraint.pl
     % for a minimally reproducible example.
     %
-    %% sum_list(Costs, TotalCost), % LINE IN QUESTION
     %
     % Not sure how it is happening but perhaps the assert statement in
     % parse_item is being run multiple times? The subset function may be broken?
@@ -114,33 +130,32 @@ parse_house_request(Sentence) :-
     % in the input).
     %
     % You're smart you'll figure it out. Have fun :)
-    %% fulfills_constraint(UnitType1, Comparison1, Value1, Items),
-    %% fulfills_constraint(UnitType2, Comparison2, Value2, Items),
-
-    write("Was able to find something that meets all the constraints"), nl,
-    write("Items: "), write(SelectedItems), nl.
+    %write("Comparison1: "), write(Comparison1), nl,
+    %write("Comparison2: "), write(Comparison2), nl,
 
 % NOTE: Not sure if any of the constraint stuff below works. I am pretty sure
 % that it does however I just havent been able to test it properly.
 fulfills_constraint(UnitType, Comparison, Value, Items) :-
     (UnitType == cost ->
-         cost_constraint(Comparison, Value, Items) ;
+         cost_constraint(Comparison, Value, Items);
          volume_constraint(Comparison, Value, Items)
     ).
 
 cost_constraint(Comparison, Value, Items) :-
-    write("Cost Constraint: "), write("Comparison: "), write(Comparison), write("Value: "), write(Value), nl,
-    findall(Cost, (member(Item, Items), item_info(ItemName, _, Cost, _)), Costs),
+    % write("Cost Constraint: "), write("Comparison: "), write(Comparison), write("Value: "), write(Value), nl,
+    % Items \= [], You should consider the empty list
+    findall(Cost, (member(Item, Items), nth0(1, Item, Cost) ), Costs),
     sum_list(Costs, TotalCost),
     (Comparison == less_than ->
-         TotalCost < Value ;
+         TotalCost < Value;
          TotalCost > Value
     ).
 volume_constraint(Comparison, Value, Items) :-
-    findall(Volume, (member(Item, Items), item_info(ItemName, _, _, Volume)), Volumes),
+    % Items \= [], you should consider the empty list
+    findall(Volume, (member(Item, Items), nth0(2, Item, Volume) ), Volumes),
     sum_list(Volumes, TotalVolume),
     (Comparison == less_than ->
-            TotalVolume < Value ;
+            TotalVolume < Value;
             TotalVolume > Value
     ).
 
