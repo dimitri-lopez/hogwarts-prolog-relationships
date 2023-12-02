13f9886df1ea9b427fc645c2d399796232cff946
dimitril <dimitrilopez01@gmail.com>
2023-12-01 22:11:37 -0500

    Adding constraint progress and notes for Dylan
    This commit runs as is but doesn't do any constraint checking. Check out
    the comments in the file for more details.

--- part2.pl
+++ part2.pl
@@ -33,9 +33,9 @@ has_item(Person, Item, Cost, Volume) -->
 house(House) --> [House], {houseOf(House, _)}.
 comparison(less_than) --> [less, than].
 comparison(greater_than) --> [greater, than].
-attribute(money) --> [total, price].
+attribute(cost) --> [total, price].
 attribute(volume) --> [total, volume].
-unit(money) --> [dollars].
+unit(cost) --> [dollars].
 unit(volume) --> [cubic, feet].
 attribute_value(UnitType, Comparison, Value) --> attribute(UnitType), comparison(Comparison), value(Value), unit(UnitType).
 house_request(House, UnitType1, Comparison1, Value1, UnitType2, Comparison2, Value2) -->
@@ -59,6 +59,13 @@ parse_item(Sentence) :-
     ),
     write("Was able to parse item!"), nl.
 
+% Generate all subsets of a list
+subset([], []).
+subset([H|T], [H|T1]):-
+  subset(T,T1).
+subset([_|T],T1):-
+  subset(T, T1).
+
 parse_house_request(Sentence) :-
     phrase(house_request(House, UnitType1, Comparison1, Value1,
                          UnitType2, Comparison2, Value2),
@@ -70,7 +77,72 @@ parse_house_request(Sentence) :-
     write("UnitType2: "), write(UnitType2), nl,
     write("Comparison2: "), write(Comparison2), nl,
     write("Value2: "), write(Value2), nl,
-    write("Was able to parse house request!"), nl.
+    write("Was able to parse house request!"), nl,
+
+    findall([Item, Person, Cost, Volume], item_info(Item, Person, Cost, Volume), AllItems),
+    write("AllItems: "), write(AllItems), nl,
+    subset(AllItems, SelectedItems),
+    findall(Cost, (member(Item, SelectedItems), nth0(2, Item, Cost)), Costs),
+    write("Costs: "), write(Costs), nl,
+    % NOTE: All I want to do on this line is sum up the costs in a list. Not
+    % sure why uncommenting this line causes AllItems to infinitely grow in
+    % size. Figure out this bug and you are basically done. See constraint.pl
+    % for a minimally reproducible example.
+    %
+    %% sum_list(Costs, TotalCost), % LINE IN QUESTION
+    %
+    % Not sure how it is happening but perhaps the assert statement in
+    % parse_item is being run multiple times? The subset function may be broken?
+    % Though when trying it with constraint.pl it works fine as far as I can
+    % tell. idk just some thoughts that I have had.
+    %
+    % Another thing to note. The findall state above creating AllItems is likely
+    % storing way more information than it needs to. I think all it needs is the
+    % ItemName and then in the constraint satisfaction you can all the items
+    % using item_info(ItemName, _, _, _)
+    %
+    % Last little things in my brain noggin:
+    % 1. There are multiple items with the same name but with different
+    % properties. These should be treated as different items. See pdf expected
+    % output for details
+    % 2. subset produces an empty list as one of the examples. You probably want
+    % to fix that or add another constraint that an empty list won't satisfy.
+    % 3. Something that has saved me a lot of time on this hw is to start with
+    % very small examples and then build up. I just dont know the intricasies of
+    % the language enough to do otherwise. If you start running into issues I
+    % would recommend doing likewise (like reduce the number of items and houses
+    % in the input).
+    %
+    % You're smart you'll figure it out. Have fun :)
+    %% fulfills_constraint(UnitType1, Comparison1, Value1, Items),
+    %% fulfills_constraint(UnitType2, Comparison2, Value2, Items),
+
+    write("Was able to find something that meets all the constraints"), nl,
+    write("Items: "), write(SelectedItems), nl.
+
+% NOTE: Not sure if any of the constraint stuff below works. I am pretty sure
+% that it does however I just havent been able to test it properly.
+fulfills_constraint(UnitType, Comparison, Value, Items) :-
+    (UnitType == cost ->
+         cost_constraint(Comparison, Value, Items) ;
+         volume_constraint(Comparison, Value, Items)
+    ).
+
+cost_constraint(Comparison, Value, Items) :-
+    write("Cost Constraint: "), write("Comparison: "), write(Comparison), write("Value: "), write(Value), nl,
+    findall(Cost, (member(Item, Items), item_info(ItemName, _, Cost, _)), Costs),
+    sum_list(Costs, TotalCost),
+    (Comparison == less_than ->
+         TotalCost < Value ;
+         TotalCost > Value
+    ).
+volume_constraint(Comparison, Value, Items) :-
+    findall(Volume, (member(Item, Items), item_info(ItemName, _, _, Volume)), Volumes),
+    sum_list(Volumes, TotalVolume),
+    (Comparison == less_than ->
+            TotalVolume < Value ;
+            TotalVolume > Value
+    ).
 
 % Hints:
 %   for NLP parser make sure you match the type of the atom: 
